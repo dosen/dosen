90a9ba28e31bcc5c647048056871c97f01678da7
Kaito Udagawa <umireon@gmail.com>
2014-10-28 14:09:06 +0900

    Use jQuery

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
+var bower = require('gulp-bower');
 var gulp = require('gulp');
 var typescript = require('gulp-tsc');
 
-gulp.task('compile', function() {
-  gulp.src(['public/*.ts'])
+gulp.task('default', ['compile']);
+
+gulp.task('compile', ['bower', 'typescript']);
+gulp.task('test', ['compile']);
+
+gulp.task('bower', function() {
+  return bower();
+});
+
+gulp.task('typescript', function() {
+  return gulp.src(['public/*.ts'])
     .pipe(typescript())
     .pipe(gulp.dest('public/'));
 });
-
-gulp.task('default', ['compile']);
-gulp.task('test', ['compile']);
 
