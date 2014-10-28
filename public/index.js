3337fe44249d50aaa433109789219d073fee6a8f
Kaito Udagawa <umireon@gmail.com>
2014-10-28 14:43:25 +0900

    Simple request to Wikipedia with JSONP Expected jQuery to sandboxing properly

--- public/index.js
+++ public/index.js
@@ -1,2 +1,9 @@
 /// <reference path="typings/jquery/jquery.d.ts" />
-console.log(jQuery("h1").text());
+var submit = document.querySelector('input[type=submit]');
+var search = document.querySelector('input[type=text]');
+submit.addEventListener('click', function () {
+    var q = search.value;
+    jQuery.getJSON("http://ja.wikipedia.org/w/api.php?action=query&titles=DVD&format=json&callback=?", function (data) {
+        search.value = data;
+    });
+});
