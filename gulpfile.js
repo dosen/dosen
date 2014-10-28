cf04899b170ac0637b4488a86269d7a1a855ee7c
Kaito Udagawa <umireon@gmail.com>
2014-10-28 14:16:40 +0900

    DefinitelyTyped installed

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 var bower = require('gulp-bower');
 var gulp = require('gulp');
+var tsd = require('gulp-tsd');
 var typescript = require('gulp-tsc');
 
 gulp.task('default', ['compile']);
 
-gulp.task('compile', ['bower', 'typescript']);
+gulp.task('compile', ['bower', 'tsd', 'typescript']);
 gulp.task('test', ['compile']);
 
 gulp.task('bower', function() {
   return bower();
+});
+
+gulp.task('tsd', function() {
+  return gulp.src('./gulp_tsd.json')
+    .pipe(tsd());
 });
 
 gulp.task('typescript', function() {
