64fc9e0e4826c9b0f8407a2bd0af730ff60e0fed
Kaito Udagawa <umireon@gmail.com>
2014-11-04 23:09:51 +0900

    Support for autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var autoprefixer = require('gulp-autoprefixer');
 var bower = require('gulp-bower');
 var gulp = require('gulp');
 var livereload = require('gulp-livereload');
@@ -10,7 +11,7 @@ var paths = {
 
 gulp.task('default', ['compile']);
 
-gulp.task('compile', ['bower', 'typescript']);
+gulp.task('compile', ['bower', 'css', 'typescript']);
 gulp.task('test', ['compile']);
 gulp.task('watch', function() {
   gulp.watch(paths.ts, ['typescript']);
@@ -23,9 +24,14 @@ gulp.task('bower', function() {
   return bower();
 });
 
+gulp.task('css', function() {
+  return gulp.src('css/*.css')
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('public/css'));
+});
+
 gulp.task('typescript', function() {
   return gulp.src(paths.ts)
     .pipe(typescript({safe: true}))
     .pipe(gulp.dest('public/'));
 });
-
