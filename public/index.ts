b240b6f96f4a0b4740848fb498cd8e4475eb53d8
Kaito Udagawa <umireon@gmail.com>
2014-11-04 23:52:12 +0900

    Arrow Descripter

--- public/index.ts
+++ public/index.ts
@@ -4,6 +4,7 @@
 var submit = <HTMLInputElement>document.querySelector('input[type=submit]');
 var search = <HTMLInputElement>document.querySelector('input[type=text]');
 var desc = <HTMLTextAreaElement>document.querySelector('textarea');
+var metric1_icon = <HTMLElement>document.querySelector('.metric__icon');
 submit.addEventListener('click', function() {
   var url = 'http://ja.wikipedia.org/w/api.php?callback=?';
   jQuery.ajax({
@@ -24,7 +25,6 @@ submit.addEventListener('click', function() {
     for (var k in pages) {
       var rev = pages[k]['revisions'][0];
       var text = rev['*'];
-      desc.innerText = text;
       var mya = Extractor.extract(text);
       showMyAnimal(mya);
       break;
@@ -44,8 +44,17 @@ submit.addEventListener('click', function() {
 });
 
 function showMyAnimal(animal: Animal) {
-  var bl = document.getElementById('my-bodyLength');
-  bl.innerText = animal.bodyLength.toString();
-  var bw = document.getElementById('my-bodyWeight');
-  bw.innerText = animal.bodyWeight.toString();
+  var m1t = document.querySelector('#metric-1 .metric__title');
+  m1t.textContent = "bodyLength";
+  var m1v = document.querySelector('#metric-1 .metric__value');
+  m1v.textContent = animal.bodyLength.toString();
+  var m1i = <HTMLElement>document.querySelector('#metric-1 .metric__icon');
+  m1i.classList.add('metric__icon--down');
+
+  var m2t = document.querySelector('#metric-2 .metric__title');
+  m2t.textContent = "bodyWeight";
+  var m2v = document.querySelector('#metric-2 .metric__value');
+  m2v.textContent = animal.bodyWeight.toString();
+  var m2i = <HTMLElement>document.querySelector('#metric-2 .metric__icon');
+  m2i.classList.add('metric__icon--up');
 }
