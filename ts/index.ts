1f313fb290f8d009a451e41bf2582147bb6515d4
Kaito Udagawa <umireon@gmail.com>
2014-11-05 14:53:03 +0900

    2nd pane to compete

--- ts/index.ts
+++ ts/index.ts
@@ -1,39 +1,69 @@
-var submit = <HTMLInputElement>document.querySelector('input[type=submit]');
-var search = <HTMLInputElement>document.querySelector('input[type=text]');
-var desc = <HTMLTextAreaElement>document.querySelector('textarea');
-var metric1_icon = <HTMLElement>document.querySelector('.metric__icon');
-submit.addEventListener('click', function() {
-  var url = 'http://ja.wikipedia.org/w/api.php?callback=?';
-  var wp = new Wikipedia();
-  wp.getText(search.value).done(function(text) {
-    var mya = Extractor.extract(text);
-    showMyAnimal(mya);
-  });
-  /*jQuery.getJSON(url, {
-    action: 'query',
-    format: 'json',
-    list: 'backlinks',
-    bltitle: search.value,
-    bllimit: 500
-  }).done(function(data) {
-    var backlinks = data['query']['backlinks'];
-    var bl = document.getElementById('my-backlinks');
-    bl.innerText = backlinks.length;
-  });*/
-});
+var panes = document.getElementsByClassName('pane');
+for (var i = 0; i < panes.length; i++) {
+  var pane = <HTMLElement>panes[i];
+  console.assert(pane != undefined);
+  (function(pane) {
+    var submit = <HTMLInputElement>pane.querySelector('input[type=submit]');
+    console.assert(submit != null);
 
-function showMyAnimal(animal: Animal) {
-  var m1t = document.querySelector('#metric-1 .metric__title');
-  m1t.textContent = "bodyLength";
-  var m1v = document.querySelector('#metric-1 .metric__value');
-  m1v.textContent = animal.bodyLength.toString();
-  var m1i = <HTMLElement>document.querySelector('#metric-1 .metric__icon');
-  m1i.classList.add('metric__icon--down');
+    var search = <HTMLInputElement>pane.querySelector('input[type=text]');
+    console.assert(search != null);
 
-  var m2t = document.querySelector('#metric-2 .metric__title');
-  m2t.textContent = "bodyWeight";
-  var m2v = document.querySelector('#metric-2 .metric__value');
-  m2v.textContent = animal.bodyWeight.toString();
-  var m2i = <HTMLElement>document.querySelector('#metric-2 .metric__icon');
-  m2i.classList.add('metric__icon--up');
+    submit.addEventListener('click', function() {
+      var url = 'http://ja.wikipedia.org/w/api.php?callback=?';
+      var wp = new Wikipedia();
+      wp.getText(search.value).done(function(text) {
+        var animal = Extractor.extract(text);
+
+        var metric_1 = document.getElementById(pane.id + '__metric_1');
+        console.assert(metric_1 != null);
+
+        var title = <HTMLElement>metric_1.querySelector('.metric__title');
+        title.textContent = "bodyLength";
+
+        var value = <HTMLElement>metric_1.querySelector('.metric__value');
+        value.textContent = animal.bodyLength.toString();
+
+        var icon = <HTMLElement>metric_1.querySelector('.metric__icon');
+        if (Math.random() > 0.5) {
+          icon.classList.add('metric__icon--up');
+        } else {
+          icon.classList.add('metric__icon--down');
+        }
+
+        var metric_2 = document.getElementById(pane.id + '__metric_2');
+        console.assert(metric_2 != null);
+
+        var title = <HTMLElement>metric_2.querySelector('.metric__title');
+        title.textContent = "bodyWeight";
+
+        var value = <HTMLElement>metric_2.querySelector('.metric__value');
+        value.textContent = animal.bodyWeight.toString();
+
+        var icon = <HTMLElement>metric_2.querySelector('.metric__icon');
+        if (Math.random() > 0.5) {
+          icon.classList.add('metric__icon--up');
+        } else {
+          icon.classList.add('metric__icon--down');
+        }
+      });
+      wp.getBacklinks(search.value).done(function(backlinks) {
+        var metric_3 = document.getElementById(pane.id + '__metric_3');
+        console.assert(metric_3 != null);
+
+        var title = <HTMLElement>metric_3.querySelector('.metric__title');
+        title.textContent = "backlinks";
+
+        var value = <HTMLElement>metric_3.querySelector('.metric__value');
+        value.textContent = backlinks.length.toString();
+
+        var icon = <HTMLElement>metric_3.querySelector('.metric__icon');
+        if (Math.random() > 0.5) {
+          icon.classList.add('metric__icon--up');
+        } else {
+          icon.classList.add('metric__icon--down');
+        }
+      });
+    });
+  })(pane);
 }
--- ts/wikipedia.ts
+++ ts/wikipedia.ts
@@ -2,7 +2,7 @@
 class Wikipedia {
   public endpoint = 'http://ja.wikipedia.org/w/api.php?callback=?';
 
-  public getText(title: string): JQueryPromise<(string)=>void> {
+  public getText(title: string) {
     console.debug('getting from Wikipadia the text of ' + title);
     return jQuery.getJSON(this.endpoint, {
         action: 'query',
@@ -12,53 +12,50 @@ class Wikipedia {
         titles: title,
         redirects: true
     }).then(function(data) {
-      var pages = data['query']['pages'];
+      console.assert('query' in data);
+      var query = data['query'];
+
+      console.assert('pages' in query)
+      var pages = query['pages'];
       for (var k in pages) {
         console.assert(k in pages);
         var page = pages[k];
 
-        console.assert('revisions' in page);
+        if ( ! ('revisions' in page)) {
+          console.warn('cannot find the text of ' + title);
+          return $.Deferred().reject('');
+        }
         var revs = page['revisions'];
 
         console.assert(0 in revs);
         var rev = revs[0];
 
         console.assert('*' in rev);
-        var text = rev['*'];
+        var text = <string>rev['*'];
 
         console.debug('retrieved the text of ' + title);
-        return text;
+        return $.Deferred().resolve(text);
       }
     });
   }
-}
-/*
-  jQuery.getJSON(url, {
-    action: 'query',
-    format: 'json',
-    list: 'backlinks',
-    bltitle: search.value,
-    bllimit: 500
-  }).done(function(data) {
-    var backlinks = data['query']['backlinks'];
-    var bl = document.getElementById('my-backlinks');
-    bl.innerText = backlinks.length;
-  });
-});
 
-function showMyAnimal(animal: Animal) {
-  var m1t = document.querySelector('#metric-1 .metric__title');
-  m1t.textContent = "bodyLength";
-  var m1v = document.querySelector('#metric-1 .metric__value');
-  m1v.textContent = animal.bodyLength.toString();
-  var m1i = <HTMLElement>document.querySelector('#metric-1 .metric__icon');
-  m1i.classList.add('metric__icon--down');
+  public getBacklinks(title: string) {
+    console.debug('getting from Wikipadia the backlinks of ' + title);
+    return jQuery.getJSON(this.endpoint, {
+      action: 'query',
+      format: 'json',
+      list: 'backlinks',
+      bltitle: title,
+      bllimit: 500
+    }).then(function(data) {
+      console.assert('query' in data);
+      var query = data['query'];
 
-  var m2t = document.querySelector('#metric-2 .metric__title');
-  m2t.textContent = "bodyWeight";
-  var m2v = document.querySelector('#metric-2 .metric__value');
-  m2v.textContent = animal.bodyWeight.toString();
-  var m2i = <HTMLElement>document.querySelector('#metric-2 .metric__icon');
-  m2i.classList.add('metric__icon--up');
+      console.assert('backlinks' in query)
+      var backlinks = query['backlinks'];
+
+      console.debug('retrieved the backlinks of ' + title);
+      return $.Deferred().resolve(backlinks);
+    });
+  }
 }
-*/
