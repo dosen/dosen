ccd254892f8061c81dfc8d936036fe14ca00de34
fukuda futoshi <ff_ltstkfrakn@yahoo.co.jp>
2014-11-07 12:11:12 +0900

    add new metrics : -1powers and nameLength

--- ts/extractor.ts
+++ ts/extractor.ts
@@ -35,4 +35,50 @@ class Extractor {
       }
     }
   }
+  
+  static recBodyLength(text){
+    var patterns = [
+      {re: /([.0-9]+)(?:m|メートル)/, exp: 1},
+      {re: /([.0-9]+)(?:cm|センチメートル)/, exp: 1e-2},
+      {re: /([.0-9]+)(?:mm|ミリメートル)/, exp: 1e-3}
+    ];
+    for (var i = 0; i < patterns.length; i++) {
+      var p = patterns[i];
+      var m = p.re.exec(text);
+      if (m != null) {
+        return 1/(parseInt(m[1]) * p.exp);
+      }
+    }
+  }
+
+  static recBodyWeight(text) {
+    var patterns = [
+      {re: /([.0-9]+)(?:g|グラム)/, exp: 1e-3},
+      {re: /([.0-9]+)(?:kg|キログラム)/, exp: 1},
+      {re: /([.0-9]+)(?:t|トン)/, exp: 1e3}
+    ];
+    for (var i = 0; i < patterns.length; i++) {
+      var p = patterns[i];
+      var m = p.re.exec(text);
+      if (m != null) {
+        return 1/(parseInt(m[1]) * p.exp);
+      }
+    }
+  }
+
+  static nameLength(text){
+    var pattern = /(?:名称 =)([ァ-ン]+)/;
+    var m = pattern.exec(text);
+    if (m != null) {
+      return m[1].length;
+    }
+  }
+
+  static recNameLength(text){
+    var pattern = /(?:名称 =)([ァ-ン]+)/;
+    var m = pattern.exec(text);
+    if (m != null) {
+      return 1/(m[1].length);
+    }
+  }
 }
