f2e1572c18efbf9205d66b2db0dbbe1d5f12e592
Kaito Udagawa <umireon@gmail.com>
2014-11-08 16:49:58 +0900

    deploy with harp

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,17 @@
-var autoprefixer = require('gulp-autoprefixer');
-var bower = require('gulp-bower');
 var gulp = require('gulp');
-var livereload = require('gulp-livereload');
 var spawn = require('child_process').spawn;
 var typescript = require('gulp-tsc');
 
 var paths = {
-  css: 'css/*.css',
   ts: 'ts/*.ts'
 }
 
-gulp.task('default', ['compile']);
+gulp.task('default', ['typescript']);
 
-gulp.task('compile', ['bower', 'css', 'typescript']);
-gulp.task('test', ['compile']);
+gulp.task('test', ['typescript']);
 gulp.task('watch', function() {
   gulp.watch(paths.ts, ['typescript']);
-  gulp.watch(paths.css, ['css']);
-  livereload.listen();
-  gulp.watch('public/*').on('change', livereload.changed);
   spawn('npm', ['start']);
-});
-
-gulp.task('bower', function() {
-  return bower();
-});
-
-gulp.task('css', function() {
-  return gulp.src(paths.css)
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('public/'));
 });
 
 gulp.task('typescript', function() {
