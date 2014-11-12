ba6e1875c925b2d7757ca2d603c255a761189c92
Kaito Udagawa <umireon@gmail.com>
2014-11-12 20:41:45 +0900

    harp server with PORT

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var harp = require("harp")
-var dirPath = __dirname + "/public";
+var dirPath = __dirname;
 var port = process.env.PORT || 9000;
 harp.server(dirPath, { port: port }, function () {
   console.log("listening at http://localhost:" + port);
