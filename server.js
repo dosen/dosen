8774a21d0e3c423d58951771068b910ee12b63e2
Kaito Udagawa <umireon@gmail.com>
2014-11-12 20:46:49 +0900

    remove server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-var harp = require("harp")
-var dirPath = __dirname;
-var port = process.env.PORT || 9000;
-harp.server(dirPath, { port: port }, function () {
-  console.log("listening at http://localhost:" + port);
-});
