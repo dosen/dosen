860ea39a082b96ef4a0c9041a2097ec90dff2926
Kaito Udagawa <umireon@gmail.com>
2014-11-26 11:33:41 +0900

    test for metric.BodyWeight

--- public/tests/integration/metric_test.ts
+++ public/tests/integration/metric_test.ts
@@ -18,23 +18,68 @@ describe("metric", function(): void {
     it("should get a 'm' metric", function(done: MochaDone): void {
       this.timeout(5000);
       m.getMetric("ペリカン").then(function(mi: metric.IMetricItem): void {
-        expect(mi).to.have.property("name").and.equal("体長");
-        expect(mi).to.have.property("text").and.equal("170センチメートル");
-        expect(mi).to.have.property("value").and.equal(1.7);
-        done();
+      	try {
+          expect(mi).to.have.property("name").and.equal("体長");
+          expect(mi).to.have.property("text").and.equal("170センチメートル");
+          expect(mi).to.have.property("value").and.equal(1.7);
+          done();
+        } catch (e) {
+          done(e);
+        }
       });
     });
 
     it("should get some value for invalid text", function(done: MochaDone): void {
       this.timeout(5000);
-      m.getMetric("JavaScript").then(function(mi: metric.IMetricItem): void {
-        expect(mi).to.have.property("name").and.equal("体長");
-        expect(mi).to.have.property("text");
-        expect(mi).to.have.property("value");
-        done();
+      m.getMetric("ストリング").then(function(mi: metric.IMetricItem): void {
+      	try {
+          expect(mi).to.have.property("name").and.equal("体長");
+          expect(mi).to.have.property("text");
+          expect(mi).to.have.property("value");
+          done();
+        } catch (e) {
+          done(e);
+        }
       });
     });
   });
+
+  describe("BodyWeight", function(): void {
+    var m: metric.IMetric;
+
+    beforeEach(function(): void {
+      m = new metric.BodyWeight(wikipedia);
+    });
+
+    it("should get a 'm' metric", function(done: MochaDone): void {
+      this.timeout(5000);
+      m.getMetric("ペリカン").then(function(mi: metric.IMetricItem): void {
+      	try {
+          expect(mi).to.have.property("name").and.equal("体重");
+          expect(mi).to.have.property("text").and.equal("11キログラム");
+          expect(mi).to.have.property("value").and.equal(11);
+          done();
+        } catch (e) {
+          done(e);
+        }
+      });
+    });
+
+    it("should get some value for invalid text", function(done: MochaDone): void {
+      this.timeout(5000);
+      m.getMetric("ストリング").then(function(mi: metric.IMetricItem): void {
+      	try {
+          expect(mi).to.have.property("name").and.equal("体重");
+          expect(mi).to.have.property("text");
+          expect(mi).to.have.property("value").and.equal(0);
+          done();
+        } catch (e) {
+          done(e);
+        }
+      });
+    });
+  });
+
 
   /*
   describe("#bodyWeight", function(): void {
