532cf1de7f7a4ec6edcf465043454cbb0cd9f867
Kaito Udagawa <umireon@gmail.com>
2014-12-02 22:45:42 +0900

    add TaxonomyImage and tests

--- public/metric.ts
+++ public/metric.ts
@@ -125,4 +125,34 @@ module metric {
       return metric;
     }
   }
+
+  export class TaxonomyImage implements IMetric {
+    public name = "画像";
+    public defaultText = "NO IMAGE";
+    public defaultValue = 0;
+
+    constructor(private wikipedia: wp.Wikipedia) {
+    }
+
+    public getMetric(name: string): ng.IPromise<IMetricItem> {
+      var wikipedia = this.wikipedia;
+      return this.wikipedia.getText(name)
+      .then(function (text: string): ng.IPromise<string> {
+        var m = /(?:画像)[^\[]*\[\[([^|]*)\|/.exec(text);
+        if (m != null) {
+          return wikipedia.getImage(m[1]);
+        } else {
+          return null;
+        }
+      })
+      .then(function(url: string): IMetricItem {
+        console.log(url);
+        return {
+          name: "画像",
+          text: url,
+          value: 0
+        };
+      });
+    }
+  }
 }
