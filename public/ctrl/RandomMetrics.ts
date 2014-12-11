9b7d17501c7bbcdddcd42383c7b4efb22d32c405
Kaito Udagawa <umireon@gmail.com>
2014-12-11 23:56:02 +0900

    add RandomMetrics

--- public/ctrl/RandomMetrics.ts
+++ public/ctrl/RandomMetrics.ts
@@ -14,7 +14,8 @@ module ctrl {
       [
         { name: "Backlinks", weight: 1 },
         { name: "Punctuations", weight: 1 },
-        { name: "BitCount", weight: 1 }
+        { name: "BitCount", weight: 1 },
+        { name: "ByteCount", weight: 1 }
       ]
     ];
 
